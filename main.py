1652ddc1a82f4ee43e205b09a968f23be10d438a
alongott15 <alongott15@gmail.com>
2024-06-15 21:51:31 +0300

    update the train and test functions

--- main.py
+++ main.py
@@ -19,10 +19,23 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 best_acc = 0
 start_epoch = 0
 
-train_set = torchvision.datasets.CIFAR10(root='./data', train=True, download=True)
+print('==> Preparing data..')
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+train_set = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
 train_loader = torch.utils.data.DataLoader(train_set, batch_size=128, shuffle=True, num_workers=2)
 
-test_set = torchvision.datasets.CIFAR10(root='./data', train=False, download=True)
+test_set = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
 test_loader = torch.utils.data.DataLoader(test_set, batch_size=100, shuffle=False, num_workers=2)
 
 net = DenseNet.densenet121()
